1434a65ff05c011aae3110babf6be0eddf429af9
Gerardo Garcia L <gerardoagl96@gmail.com>
2021-07-25 14:12:53 -0400

    Configuration for CI/CD workflow

---
+++ knexfile.ts
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  [process.env.NODE_ENV as string]: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      socketPath: process.env.DB_SOCKET_PATH,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      extension: 'ts',
+    },
+    seeds: {
+      directory: process.env.DB_SEEDERS_DIR // './seeds/test'
+    }
+  }
+};
